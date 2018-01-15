883135f2ab207c5d6762e3b269ba5f035c58dd6f
chenchao <chenchao@eastmoney.com>
2018-01-15 18:02:46 +0800

    fix

--- core/order.js
+++ core/order.js
@@ -1,4 +1,15 @@
 /**
  * Created by ChenChao on 2018/1/15.
  * 下单买票
+ * cookie 的获取使用PC登录后，在requestHead里获取
  */
+
+var request = require('request');
+var j = request.jar();
+var cookie = request.cookie('key1=value1');
+var url = 'http://www.google.com';
+j.setCookie(cookie, url);
+
+request({url: url, jar: j}, function () {
+    request('http://images.google.com')
+});
